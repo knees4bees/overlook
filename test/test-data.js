19af1245e5ba4cc235a9c585db1e25090aa4eeb3
Katie Bee <knees4bees@gmail.com>
2021-03-07 11:52:07 -0700

    Create test data

---
+++ test/test-data.js
@@ -0,0 +1,83 @@
+// ***** Customers *****
+export const customer1 = {
+  id: 1,
+  name: 'Bob Bozo'
+};
+
+export const customer2 = {
+  id: 2,
+  name: 'Kelly Conman'
+};
+
+export const customer3 = {
+  id: 3,
+  name: 'Roger Dodger'
+};
+
+export const customer4 = {
+  id: 4,
+  name: 'Nina Dreama'
+};
+
+
+// ***** Rooms *****
+export const room1 = {
+  number: 1,
+  roomType: 'residential suite',
+  bidet: true,
+  bedSize: 'queen',
+  numBeds: 1,
+  costPerNight: 358.4
+};
+
+export const room2 = {
+  number: 2,
+  roomType: 'suite',
+  bidet: false,
+  bedSize: 'full',
+  numBeds: 2,
+  costPerNight: 477.38
+};
+
+export const room3 = {
+  number: 3,
+  roomType: 'single room',
+  bidet: false,
+  bedSize: 'king',
+  numBeds: 1,
+  costPerNight: 491.14
+};
+
+
+// ***** Bookings *****
+export const booking1 = {
+  id: '0test5fwrgu4i7k55hl6sz',
+  userID: 2,
+  date: '2020/04/22',
+  roomNumber: 1,
+  roomServiceCharges: []
+};
+
+export const booking2 = {
+  id: '0test5fwrgu4i7k55hl6t5',
+  userID: 3,
+  date: '2020/01/24',
+  roomNumber: 2,
+  roomServiceCharges: []
+};
+
+export const booking3 = {
+  id: '0test5fwrgu4i7k55hl6t6',
+  userID: 1,
+  date: '2020/01/10',
+  roomNumber: 2,
+  roomServiceCharges: []
+};
+
+export const booking4 = {
+  id: '0test5fwrgu4i7k55hl6t7',
+  userID: 1,
+  date: '2020/02/16',
+  roomNumber: 3,
+  roomServiceCharges: []
+};
