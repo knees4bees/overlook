119b264708c0602cc135a4d97ed16998a9011244
Katie Bee <knees4bees@gmail.com>
2021-03-07 16:39:49 -0700

    Start adding and passing tests for BookingsRepo

---
+++ src/BookingsRepo.js
@@ -0,0 +1,11 @@
+class BookingsRepo {
+  constructor(bookingsData, customers, rooms) {
+    this.bookings = this.createBookings();
+  }
+
+  createBookings() {
+    // TODO figure this out
+  }
+};
+
+export default BookingsRepo;
