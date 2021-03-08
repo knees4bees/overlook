41db7ee5fccb8af72589f796463c7155c5b2c2f8
Katie Bee <knees4bees@gmail.com>
2021-03-08 12:15:48 -0700

    Build and pass tests for createBookings function in scripts.js

--- src/index.js
+++ src/index.js
@@ -9,4 +9,6 @@ import './css/normalize.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
+
+import './scripts.js';
--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,29 @@
 // put DOM manipulation, fetch calls, and helper functions in here
 // (can move fetch calls out to a separate file later)
+
+import Booking from "./Booking";
+import Customer from "./Customer";
+import Room from "./Room";
+
+function createBookings(rawBookingsData, rawCustomersData, rawRoomsData) {
+  const bookings = [];
+
+  const bookingsData = rawBookingsData.bookings;
+  const customersData = rawCustomersData.customers;
+  const roomsData = rawRoomsData.rooms;
+
+  bookingsData.forEach(bookingData => {
+    const foundCustomer = customersData.find(customer => customer.id === bookingData.userID);
+    const customer = new Customer(foundCustomer);
+
+    const foundRoom = roomsData.find(room => room.number === bookingData.roomNumber);
+    const room = new Room(foundRoom);
+
+    const booking = new Booking(bookingData, customer, room);
+    bookings.push(booking);
+  })
+
+  return bookings;
+}
+
+export default createBookings;
