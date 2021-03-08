c0f5bd9dd9d81cc7b67fe5e04d0191021def8e58
Katie Bee <knees4bees@gmail.com>
2021-03-08 10:12:40 -0700

    Add and pass tests for BookingsRepo methods

--- src/BookingsRepo.js
+++ src/BookingsRepo.js
@@ -1,10 +1,23 @@
 class BookingsRepo {
-  constructor(bookingsData, customers, rooms) {
-    this.bookings = this.createBookings();
+  // New plan: have this take in an array of pre-constructed Booking instances
+  constructor(bookings) {
+    this.bookings = [...bookings];
   }
 
-  createBookings() {
-    // TODO figure this out
+  filterByCustomer(custId) {
+    const filteredBookings = this.bookings.filter(booking => booking.guest.id === custId);
+    return new BookingsRepo(filteredBookings);
+  }
+
+  calculateTotalSpent(custId) {
+    const bookings = this.filterByCustomer(custId).bookings;
+
+    const total = bookings.reduce((sum, booking) => {
+      const costPerNight = parseFloat(booking.room.costPerNight);
+      sum += costPerNight;
+      return sum;
+    }, 0);
+    return total.toFixed(2);
   }
 };
 
--- test/bookingsRepo-test.js
+++ test/bookingsRepo-test.js
@@ -29,7 +29,7 @@ describe('BookingsRepo', function() {
     booking2 = new Booking(testBooking2, customer3, room2);
     booking3 = new Booking(testBooking3, customer1, room2);
     booking4 = new Booking(testBooking4, customer1, room3);
-    bookingsRepo = new BookingsRepo(booking1, booking2, booking3, booking4);
+    bookingsRepo = new BookingsRepo([booking1, booking2, booking3, booking4]);
   });
 
   it('should be a function', function() {
@@ -43,5 +43,29 @@ describe('BookingsRepo', function() {
   it('should hold all bookings in an array', function() {
     expect(bookingsRepo.bookings).to.have.length(4);
     expect(bookingsRepo.bookings[0]).to.be.an.instanceOf(Booking);
+  });
+
+  it('should filter bookings by customer', function() {
+    const cust1BookingsRepo = bookingsRepo.filterByCustomer(1);
+    const cust2BookingsRepo = bookingsRepo.filterByCustomer(2);
+    const cust3BookingsRepo = bookingsRepo.filterByCustomer(3);
+    const filteredRepo = new BookingsRepo([booking3, booking4]);
+
+    expect(cust1BookingsRepo.bookings).to.have.length(2);
+    expect(cust1BookingsRepo).to.eql(filteredRepo);
+    expect(cust2BookingsRepo.bookings).to.have.length(1);
+    expect(cust2BookingsRepo.bookings[0]).to.eql(booking1);
+    expect(cust3BookingsRepo.bookings).to.have.length(1);
+    expect(cust3BookingsRepo.bookings[0]).to.eql(booking2);
+  });
+
+  it('should calculate the total spent by a customer for all time', function() {
+    const cust1spent = bookingsRepo.calculateTotalSpent(1);
+    const cust2spent = bookingsRepo.calculateTotalSpent(2);
+    const cust3spent = bookingsRepo.calculateTotalSpent(3);
+
+    expect(cust1spent).to.equal('968.52');
+    expect(cust2spent).to.equal('358.40');
+    expect(cust3spent).to.equal('477.38');
   });
 });
